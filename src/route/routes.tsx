57a0a67ab43e27f71efa84b777870adda55419e6
Shota <shalania1263@gmail.com>
2024-07-24 16:04:54 +0400

    fixed code

--- src/route/routes.tsx
+++ src/route/routes.tsx
@@ -1,7 +1,16 @@
 import { Navigate } from "react-router-dom";
 
-const ConditionalRoute = ({ user, children }) => {
-  return user ? <Navigate to="/analytic" /> : children;
+import {  ReactNode } from "react";
+import { User } from "firebase/auth"; // Adjust the import based on where User is defined
+
+interface ConditionalRouteProps {
+  user: User | null; // Adjust this type based on your user authentication
+  children: ReactNode; // This will cover any valid React children
+}
+
+const ConditionalRoute: React.FC<ConditionalRouteProps> = ({ user, children }) => {
+  return user ? <Navigate to="/analytic" /> : <>{children}</>;
 };
 
 export default ConditionalRoute;
+
