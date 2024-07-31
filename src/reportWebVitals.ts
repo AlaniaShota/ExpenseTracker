35c6ec3c537ca10870210ee9b08bfe6d2b1a4f32
Shota <shalania1263@gmail.com>
2024-07-31 15:28:21 +0400

    change bag

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,14 +1,15 @@
+
 type ReportHandler = (metric: any) => void;
+
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
-        import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-            getCLS(onPerfEntry);
-            getFID(onPerfEntry);
-            getFCP(onPerfEntry);
-            getLCP(onPerfEntry);
-            getTTFB(onPerfEntry);
-        });
+        onCLS(onPerfEntry);
+        onFID(onPerfEntry);
+        onFCP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
     }
 };
 
